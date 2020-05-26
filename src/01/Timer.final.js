752690cddd76d66b42cfba2dab7ec85068266dde
David Khourshid <davidkpiano@gmail.com>
2020-05-25 21:39:32 -0400

    Exercise 01 final

--- src/01/Timer.final.js
+++ src/01/Timer.final.js
@@ -2,13 +2,13 @@ import * as React from 'react';
 import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import { useMachine } from '@xstate/react';
 import { ProgressCircle } from '../ProgressCircle';
 
-// import { useMachine } from '@xstate/react';
 import { timerMachine } from './timerMachine';
 
 export const Timer = () => {
-  const [state, send] = [{}, () => {}];
+  const [state, send] = useMachine(timerMachine);
 
   const { duration, elapsed, interval } = {
     duration: 60,
@@ -19,7 +19,7 @@ export const Timer = () => {
   return (
     <div
       className="timer"
-      data-state={state.value} // Hint!
+      data-state={state.value}
       style={{
         // @ts-ignore
         '--duration': duration,
@@ -39,45 +39,24 @@ export const Timer = () => {
       <ProgressCircle />
       <div className="display">
         <div className="label">{state.value}</div>
-        <div
-          className="elapsed"
-          onClick={() => {
-            // ...
-          }}
-        >
+        <div className="elapsed" onClick={() => send({ type: 'TOGGLE' })}>
           {Math.ceil(duration - elapsed)}
         </div>
         <div className="controls">
-          {state === 'paused' && (
-            <button
-              onClick={() => {
-                // ...
-              }}
-            >
-              Reset
-            </button>
+          {state.value === 'paused' && (
+            <button onClick={() => send({ type: 'RESET' })}>Reset</button>
           )}
         </div>
       </div>
       <div className="actions">
-        {state === 'running' && (
-          <button
-            onClick={() => {
-              // ...
-            }}
-            title="Pause timer"
-          >
+        {state.value === 'running' && (
+          <button onClick={() => send({ type: 'TOGGLE' })} title="Pause timer">
             <FontAwesomeIcon icon={faPause} />
           </button>
         )}
 
-        {(state === 'paused' || state === 'idle') && (
-          <button
-            onClick={() => {
-              // ...
-            }}
-            title="Start timer"
-          >
+        {(state.value === 'paused' || state.value === 'idle') && (
+          <button onClick={() => send({ type: 'TOGGLE' })} title="Start timer">
             <FontAwesomeIcon icon={faPlay} />
           </button>
         )}
--- src/01/timerMachine.js
+++ src/01/timerMachine.js
@@ -1,23 +1,4 @@
 import { createMachine } from 'xstate';
 
-export const timerMachine = createMachine({
-  initial: 'idle',
-  states: {
-    idle: {
-      on: {
-        TOGGLE: 'running',
-      },
-    },
-    running: {
-      on: {
-        TOGGLE: 'paused',
-      },
-    },
-    paused: {
-      on: {
-        TOGGLE: 'running',
-        RESET: 'idle',
-      },
-    },
-  },
-});
+// Use the machine you created in Exercise 00
+// export const timerMachine = // ...
